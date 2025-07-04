b54bba50a07bcd02319834414d53533a84ef6168
sindhu-M-Valerie <79721049+sindhu-M-Valerie@users.noreply.github.com>
2025-07-04 13:48:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReflectoCat from "./pages/ReflectoCat";
 
 function App() {
-  return <ReflectoCat />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ReflectoCat />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
