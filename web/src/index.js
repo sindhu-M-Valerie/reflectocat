47e18081e567ad08c0b7fff81781b2b6fd3e2f4d
sindhu-M-Valerie <79721049+sindhu-M-Valerie@users.noreply.github.com>
2025-08-07 11:28:52 +0000

    Add ReflectoCat build files

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,5 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
